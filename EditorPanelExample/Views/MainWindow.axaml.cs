37c68dca9322c278dceb84dc1dbbf457118c9fc5
chrisyandev <czyanhome@hotmail.com>
2023-03-07 04:06:44 -0800

    create custom list box used in Add Component Flyout, force add component logic to run on click released instead of pressed, fixed scroll to end behavior

--- EditorPanelExample/Views/MainWindow.axaml.cs
+++ EditorPanelExample/Views/MainWindow.axaml.cs
@@ -1,8 +1,18 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Selection;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using EditorPanelExample.Models;
 using EditorPanelExample.ViewModels;
+using EditorPanelExample.Views.Components.Common;
+using EditorPanelExample.Views.Components.Templates;
+using ReactiveUI;
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Reactive;
+using System.Windows.Input;
 
 namespace EditorPanelExample.Views
 {
@@ -10,34 +20,43 @@ namespace EditorPanelExample.Views
     {
         private ScrollViewer _mainScrollViewer;
         private Button _addComponentButton;
-        private ListBox _addComponentListBox;
-
+        private AddComponentListBox _addComponentListBox;
+        
         public MainWindow()
         {
             InitializeComponent();
 
             _mainScrollViewer = this.FindControl<ScrollViewer>("mainScrollViewer");
             _addComponentButton = this.FindControl<Button>("addComponentButton");
-            _addComponentListBox = this.FindControl<ListBox>("addComponentListBox");
+            _addComponentListBox = this.FindControl<AddComponentListBox>("addComponentListBox");
 
+            _addComponentButton.Flyout.Opening += AddComponentFlyoutOpening;
             _addComponentListBox.PointerReleased += AddComponentListBoxPointerReleased;
+            _addComponentListBox.MyItemSelected += AddComponentMyItemSelected;
+        }
+
+        private void AddComponentFlyoutOpening(object sender, EventArgs e)
+        {
+            _addComponentListBox.SelectedItem = null;
         }
 
         private void AddComponentListBoxPointerReleased(object sender, PointerReleasedEventArgs e)
         {
-            HideAddComponentFlyout();
-            _addComponentListBox.UnselectAll();
-            ScrollToBottom();
-        }
-
-        public void HideAddComponentFlyout()
-        {
             _addComponentButton.Flyout.Hide();
         }
 
-        public void ScrollToBottom()
+        // Assumes component was added causing a layout update
+        // Otherwise event handler will remain subscribed until a random layout update
+        private void AddComponentMyItemSelected(object sender, EventArgs e)
+        {
+            _mainScrollViewer.LayoutUpdated += ScrollViewerLayoutUpdated;
+        }
+
+        // Scrolls to end assuming a component was appended
+        private void ScrollViewerLayoutUpdated(object sender, EventArgs e)
         {
             _mainScrollViewer.ScrollToEnd();
+            _mainScrollViewer.LayoutUpdated -= ScrollViewerLayoutUpdated;
         }
     }
 }
